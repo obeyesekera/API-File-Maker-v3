2bc6db9894d075cf87601c0ec25dfad933c53109
chamikanuwan <chamikanuwan@gmail.com>
2025-02-05 19:44:06 +0530

    Singleton design pattern implemented

--- PNR-File-Maker/Program.cs
+++ PNR-File-Maker/Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -11,13 +12,33 @@ namespace PNR_File_Maker
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
+        /// 
+        // Mutex to ensure only one instance runs
+        private static Mutex mutex = null;
+
         [STAThread]
         static void Main()
         {
+            const string appName = "API_File_Maker";
+            bool createdNew;
+
+            // Create a new mutex and check if it is already created
+            mutex = new Mutex(true, appName, out createdNew);
+
+            if (!createdNew)
+            {
+                // If the mutex is already created, it means another instance is running
+                MessageBox.Show("An instance of API FILE MAKER is already running.");
+                return;
+            }
+
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new frmMain());
+
+            // Release the mutex when the application exits
+            GC.KeepAlive(mutex);
         }
     }
 }
